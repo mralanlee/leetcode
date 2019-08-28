708bb2d14dae63643e363ff6b192944db3827111
Alan Lee <alalee@zendesk.com>
2019-08-28 12:37:17 -0700

    test: wip

--- javascript/merge_two_sorted_lists/merged_two_sorted_lists.test.js
+++ javascript/merge_two_sorted_lists/merged_two_sorted_lists.test.js
@@ -1,21 +1,20 @@
 const test = require("ava");
 const mergeTwoLists = require("./merge_two_sorted_lists");
 
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+function ListNode (val){
+  this.val = val;
+  this.next = null;
 }
 
+// Singular
+// const createNode = val => new ListNode(val);
 const createList = (data = []) => {
   const list = new ListNode(data[0]);
   for (let i = 1; i < data.length; i++) {
     const newNode = new ListNode(data[i]);
 
     if (list.next !== null) {
-      list.next = newNode;
-
+      list.next.next = newNode;
     } else {
       list.next = newNode;
     }
@@ -28,7 +27,24 @@ test("test case 1", async t => {
   const listTwo = createList([1, 3, 4]);
 
   const actual = mergeTwoLists(listOne, listTwo);
-  const expected = createList([1, 1, 2, 3, 4, 4]);
 
-  t.deepEqual(actual, expected);
+  const expected = new ListNode(1);
+  expected.next = {
+      ...new ListNode(1),
+      next: {
+        ...new ListNode(2),
+        next: {
+          ...new ListNode(3),
+          next: {
+            ...new ListNode(4),
+            next: {
+              ...new ListNode(5),
+              next: null
+            }
+          }
+        }
+      }
+    };
+
+  t.is(actual, expected);
 });
